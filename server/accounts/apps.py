002c7ee7f5a80d7ed0ae97b37e18cddbcf0d5403
Yuta <y.izumi@4nonome.com>
2023-01-25 15:31:47 +0900

    add accounts_app

--- server/accounts/apps.py
+++ server/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class SampleConfig(AppConfig):
+class AccountsConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "sample"
+    name = "accounts"
--- server/mysite/settings.py
+++ server/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts.apps.AccountsConfig",
     "rest_framework",
     "rest_framework.authtoken",
     "corsheaders",
