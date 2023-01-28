5f818510cb533e2582f9264d7502384102a07e23
Yuta <y.izumi@4nonome.com>
2023-01-28 17:42:36 +0900

    Create migration files

--- server/models/school.py
+++ server/models/school.py
@@ -5,6 +5,6 @@ from django.db import models
 
 class School(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField(verbose_name="学校名")
+    name = models.CharField(verbose_name="学校名", max_length=256)
     created_at = models.DateTimeField(verbose_name="登録日時", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
--- server/mysite/urls.py
+++ server/mysite/urls.py
@@ -18,7 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
     path("api-auth/", include("rest_framework.urls")),
     path("api/v1/", include("apiv1.urls")),
 ]
