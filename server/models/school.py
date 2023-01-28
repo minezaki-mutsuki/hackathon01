6ac8082b23bf0aa9013a470b05df0f1633ff2d21
Yuta <y.izumi@4nonome.com>
2023-01-28 18:00:24 +0900

    Modify models

--- server/models/school.py
+++ server/models/school.py
@@ -1,10 +1,8 @@
-import uuid
-
 from django.db import models
 
 
 class School(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.IntegerField(primary_key=True, editable=False)
     name = models.CharField(verbose_name="学校名", max_length=256)
     created_at = models.DateTimeField(verbose_name="登録日時", auto_now_add=True)
     updated_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
