4424f0b00a118421b91789d18cc5387c1e8b6552
Yuta <y.izumi@4nonome.com>
2023-01-28 22:23:58 +0900

    Add school data to DB

--- server/models/school.py
+++ server/models/school.py
@@ -4,5 +4,3 @@ from django.db import models
 class School(models.Model):
     id = models.IntegerField(primary_key=True, editable=False)
     name = models.CharField(verbose_name="学校名", max_length=256)
-    created_at = models.DateTimeField(verbose_name="登録日時", auto_now_add=True)
-    updated_at = models.DateTimeField(verbose_name="更新日時", auto_now=True)
